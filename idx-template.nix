4ce24a0d35a5d2286c110a50385075187fa37dc7
Rody Davis <rody.davis.jr@gmail.com>
2024-11-07 15:47:40 -0800

    Update idx-template.nix

--- idx-template.nix
+++ idx-template.nix
@@ -10,10 +10,10 @@
         mkdir "$out"
         cp -rf ${./.} "$WS_NAME"
         chmod -R +w "$WS_NAME"
-        mv "$WS_NAME" "$out"/
-        rm "$out"/idx-template.nix
-        rm "$out"/idx-template.json
-        rm "$out"/dev.nix
+        mv "$WS_NAME" "$out/"
+        rm "$out/idx-template.nix"
+        rm "$out/idx-template.json"
+        rm "$out/dev.nix"
         mkdir -p "$out/flutter"
         git clone "${repo}" "$out/flutter/"
         mkdir "$out"/.idx
